bd89c929d322bb1d51d0bc7e77d63acfe8623ea8
vixsonyap <e0138272@u.nus.edu>
2019-09-30 23:11:03 +0800

    unmerge master #1

--- LMS-war/src/java/rest/ApplicationConfig.java
+++ LMS-war/src/java/rest/ApplicationConfig.java
@@ -18,7 +18,7 @@ public class ApplicationConfig extends Application
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(rest.GroupManagementResource.class    
+        resources.add(rest.GroupManagementResource.class);    
         resources.add(rest.ModuleMountingResource.class);
         resources.add(rest.UserResource.class);
  }    
