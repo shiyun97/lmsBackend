87f8fd4d575ffa478e4a93902ebe2da0a6d55121
vixsonyap <e0138272@u.nus.edu>
2019-11-15 13:15:50 +0800

    coursepack get number of users enrolled stats

---
+++ LMS-war/src/java/datamodel/rest/RetrieveCoursepackStatistics.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datamodel.rest;
+
+import java.util.List;
+
+/**
+ *
+ * @author Asus
+ */
+public class RetrieveCoursepackStatistics {
+    List<CoursepackStatistic> items;
+
+    public List<CoursepackStatistic> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CoursepackStatistic> items) {
+        this.items = items;
+    }
+
+    public RetrieveCoursepackStatistics() {
+    }
+
+    public RetrieveCoursepackStatistics(List<CoursepackStatistic> items) {
+        this.items = items;
+    }
+    
+}
